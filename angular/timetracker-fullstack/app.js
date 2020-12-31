3b2fa74ab6a55bac7dff7b6526c034c3494960df
Kirill <mrfuzzybot@gmail.com>
2020-12-31 11:26:41 +0200

    auth functional

---
+++ angular/timetracker-fullstack/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const keys = require('./config/keys')
+const authRoutes = require('./routes/auth')
+const bodyParser = require('body-parser')
+const app = express()
+
+mongoose
+  .connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDb connected.'))
+  .catch(e => console.log('error', e))
+mongoose.set('useCreateIndex', true)
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
+
+app.use('/api/auth', authRoutes)
+
+module.exports = app
---
+++ angular/timetracker-fullstack/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://kerill:kOY5tnhDR4oTX6l6@cluster0.nyct5.mongodb.net/Timetracker',
+  //?retryWrites=true&w=majority
+  jwt: 'dev-jwt'
+}
