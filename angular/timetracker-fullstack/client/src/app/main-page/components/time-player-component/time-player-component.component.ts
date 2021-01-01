42e9919c97af87fe4526b38d85c836df935663aa
Kirill <mrfuzzybot@gmail.com>
2021-01-01 11:08:12 +0200

    start functional tracker

--- angular/timetracker-fullstack/client/src/app/main-page/components/time-player-component/time-player-component.component.ts
+++ angular/timetracker-fullstack/client/src/app/main-page/components/time-player-component/time-player-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as moment from 'moment'
+import { Moment } from 'moment'
 
 @Component({
   selector: 'app-time-player-component',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimePlayerComponentComponent implements OnInit {
 
+  playing: Boolean
+  loading: Boolean
+  timeInt: number
+  time = '00:00:00'
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loading = true
+    this.playing = false
+    setTimeout(() => {
+      this.loading = false
+    }, 1000)
   }
+
+  buttonCLick() {
+    console.log('clicked', this.playing)
+    if (this.playing) {
+      // Stop
+      this.loading = true
+      setTimeout(() => {
+        this.playing = false
+        this.loading = false
+        clearInterval(this.timeInt)
+      }, 1000)
+    } else {
+      this.time = '00:00:00'
+
+      // Timer
+      this.timeInt = setInterval(() => {
+        this.time = moment(this.time, 'HH:mm:ss').add(1, 'second').format('HH:mm:ss')
+      }, 1000)
+      this.loading = true
+      setTimeout(() => {
+        this.playing = true
+        this.loading = false
+      }, 1000)
+      // Start
+    }
+  }
+
 
 }
